84b18a7f7b8a1b832eb5614c37dc837921ec9a8a
Alberto-Ghedin <alberto.ghedin@phd.units.it>
2024-04-10 17:06:51 +0100

    adapted functions to new input file

--- IndVal_selection.r
+++ IndVal_selection.r
@@ -8,35 +8,30 @@ library(readxl)
 library(tibble)
 
 
-HOME_ <- "/mnt/d"
-file_path <- "PHD/ISPRA_20152017_Analysis"
-file_name <- "site_taxa_matrix.csv"
-sites_taxa = read.csv(paste(HOME_, file_path, file_name, sep="/"), check.names = FALSE)
+# Load data
+params <- fromJSON(file = paste(path.expand("~"), "sys_specific.json", sep = "/"))
+HOME_ <- paste(params$home, "PHD", sep = "/")
 
+sheet_names <- excel_sheets(paste(HOME_, "ISPRA_20152017_Analysis/eco_matrix_region.xlsx", sep = "/"))
 
-#make column of date objects
-sites_taxa["Date"] <- as.Date(sites_taxa$Date)
+data <- list()  # Create an empty list to store the data from each sheet
+for (sheet in sheet_names) {
+    data[[sheet]] <- read_excel(paste(HOME_, "ISPRA_20152017_Analysis/eco_matrix_region.xlsx", sep = "/"), sheet = sheet)
+}
 
 # add season column 
 seasons <- c(rep("Winter", 3), rep("Spring", 3), rep("Summer", 3), rep("Autumn", 3))
 names(seasons) <- c(1:12)
-sites_taxa$Season <- sapply(sites_taxa$Date, FUN = function(x) seasons[month(x)])
 
-sites_taxa <- sites_taxa %>% relocate(Season, .after = Date)
-
-
-regional_matrix <- list()
-for (region in unique(sites_taxa$Region)){
-  regional_matrix[[region]] <- sites_taxa %>% filter(Region == region) %>% select(-c(Region, Date, id))
-  regional_seasons <- regional_matrix[[region]]$Season
-  regional_matrix[[region]] <- regional_matrix[[region]][, -1] %>% select_if(~sum(.) != 0)
-  regional_matrix[[region]]$Season <- regional_seasons
+for (region in names(data)) {
+    data[[region]]$Season <- sapply(data[[region]]$Date, FUN = function(x) seasons[month(x)])
 }
 
-# calculate indicator value
+
 ind_val <- list()
-for (region in unique(sites_taxa$Region)){
-  ind_val[[region]] <- labdsv::indval(regional_matrix[[region]] %>% select(-c(Season)), regional_matrix[[region]]$Season)
+for (region in names(data)){
+    regional_matrix <- data[[region]]
+    ind_val[[region]] <- labdsv::indval(regional_matrix %>% select(-c(Season, id, Date)), regional_matrix$Season)
 }
 
 list_dfs <- list()
@@ -44,11 +39,12 @@ for (region in names(ind_val)){
     df <- as.data.frame(ind_val[[region]]$indval)
     df["pval"] <- ind_val[[region]]$pval
     df["max_val"] <- ind_val[[region]]$indcls
-    df <- rownames_to_column(df, var = "Taxa")
+    df <- tibble::rownames_to_column(df, var = "Taxa")
     list_dfs[[region]] <- df
 }
 
-write.xlsx(list_dfs, file = paste(HOME_, file_path, "IndVal_per_region.xlsx", sep = "/"))
+file_path <- "ISPRA_20152017_Analysis"
+openxlsx::write.xlsx(list_dfs, file = paste(HOME_, file_path, "IndVal_per_region.xlsx", sep = "/"))
 
 
 
