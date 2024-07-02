c2b5d33ff1adb6a94cabd26415aaf77630ba9fe4
Alberto-Ghedin <alberto.ghedin@pgd.units.it>
2024-07-02 09:18:45 +0200

    removing under flag values and looking for high correlations

--- Clustering/Results/Env_data_statistics.r
+++ Clustering/Results/Env_data_statistics.r
@@ -12,6 +12,10 @@ nutrients <- nutrients[-which(nutrients == "E_cond")]
 env_data <-  lapply(nutrients, function(sheet) read.xlsx(paste(HOME_, "ISPRA_20152017_Analysis/Create_dataset/df_chem_phys_mod_data_cleaned.xlsx", sep = "/"), sheet = sheet))
 names(env_data) <- nutrients
 
+
+
+
+
 index_cluster <- read.csv(paste(HOME_, "/ISPRA_20152017_Analysis/Clustering/Results/cluster_index.csv", sep = "/"))
 
 index_cluster
