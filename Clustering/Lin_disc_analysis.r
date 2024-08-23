35a932cdd960b47bdd0acbe5b480b559090f513f
Alberto-Ghedin <alberto.ghedin@phd.units.it>
2024-08-23 16:22:42 +0100

    rda on species data

--- Clustering/Lin_disc_analysis.r
+++ Clustering/Lin_disc_analysis.r
@@ -3,7 +3,6 @@ library(ggplot2)
 library(dplyr)
 library(lubridate)
 library(openxlsx)
-library(readxl)
 library(tibble)
 library(jsonlite)
 library(zoo)
@@ -11,18 +10,27 @@ library(klaR)
 library(DAAG)
 library(rstatix)
 library(ggpubr)
+library(vegan)
+library(tidyr)
 
 HOME_ <- paste(path.expand("~"), "PHD", sep = "/")
 
-training_data <- read.csv(paste(HOME_, "ISPRA_20152017_Analysis/Clustering/Unknown_effect/training_data_LDA.csv", sep = "/")) 
+env_data <- read.csv(paste(HOME_, "/ISPRA_20152017_Analysis/Create_dataset/df_chem_phys_mod_data_cleaned_long_format.csv", sep = "/")) 
+cluster_indices <- read.xlsx(paste(HOME_, "/ISPRA_20152017_Analysis/Clustering/Unknown_effect/cluster_indices.xlsx", sep = ""),sheet = "method_2", detectDates = TRUE)
 
-clustering <- "ward_4"
+
+cluster_indices$Date <- openxlsx::convertToDate(cluster_indices$Date)
+env_data$Date <- as.Date(env_data$Date)
+training_data <- merge(env_data, cluster_indices %>% select(Date, id, ward_4, ward_11), by = c("Date", "id"))
+
+clustering <- "ward_11"
 
 ### POSSIBLE CHOICES ###
 # 1) all covariates
 # 2) physical + pH + Chla 
 # 3) physical + pH + Chla + TN + SiO4
 
+#c("Chla", "pH", "NH4", "TN", "TP")
 covariates <- c(
   "T",
   "Salinity",
@@ -38,9 +46,42 @@ covariates <- c(
   "SiO4"
 )
 
-training_data %>% group_by(ward_4) %>% summarise(n = n())
+all_nutrients <- c(
+  "Chla",
+  "NO3",
+  "NO2",
+  "NH4",
+  "TN",
+  "PO4",
+  "TP",
+  "SiO4"
+)
 
-total_samples <- training_data %>% group_by(!!as.symbol(clustering)) %>% summarise(n = n())
+boxcox_transform <- function(values, min_val = 0.001) {
+  
+  if (any(is.na(values))) {
+    clean_values <- as.numeric(na.omit(values))
+  } else {
+     clean_values <- as.numeric(values)
+  }
+  if (min(clean_values) == 0.0) {
+    clean_values <- clean_values + min_val * 0.1
+  }
+
+  lambda <- boxcox(clean_values ~ 1, plotit = FALSE)
+  max_lambda <- lambda$x[which(lambda$y == max(lambda$y))]
+  if (max_lambda == 0) {
+    clean_values <- log(clean_values)
+  } else {
+    clean_values <- (clean_values^max_lambda - 1) / max_lambda
+  }
+  values[which(!is.na(values))] <- clean_values
+  return(values)
+}
+
+
+training_data %>% dplyr::select(all_of(c(clustering, covariates))) %>% summary()
+
 
 training_data %>% 
   select(all_of(c(clustering, covariates))) %>%
@@ -48,9 +89,37 @@ training_data %>%
   group_by(!!as.symbol(clustering)) %>%
   summarise(total_rows = n()) / total_samples
 
-training_data.clean <- training_data %>% select(all_of(c(clustering, covariates))) %>% na.omit()
 
-training_data.clean %>% select(all_of(covariates)) %>% base::sum()
+# Assuming training_data is your dataframe and covariates is a vector of column names
+# Reshape the data to long format
+long_data <- training_data %>%
+  select(all_of(covariates)) %>%
+  pivot_longer(cols = everything(), names_to = "variable", values_to = "value")
+
+# Create histograms for each variable
+ggplot(long_data, aes(x = value)) +
+  geom_histogram(binwidth = 30, fill = "blue", color = "black", alpha = 0.7) +
+  facet_wrap(~ variable, scales = "free_x") +
+  theme_minimal() +
+  labs(title = "Histograms of Covariates", x = "Value", y = "Frequency")
+
+
+
+
+training_data.clean <- training_data %>%
+  select(all_of(c(clustering, covariates))) %>%
+  na.omit() %>%
+  mutate(across(all_of(covariates), ~ decostand(boxcox_transform(.), "standardize"))) 
+
+
+ggplot(training_data.clean %>%
+  select(all_of(covariates)) %>%
+  pivot_longer(cols = everything(), names_to = "variable", values_to = "value"), aes(x = value)) +
+  geom_histogram(fill = "blue", color = "black", alpha = 0.7) +
+  facet_wrap(~ variable, scales = "free_x") +
+  theme_minimal() +
+  labs(title = "Histograms of Covariates", x = "Value", y = "Frequency")
+
 ## MANOVA TEST ##
 
 # testing for normality of each covariates within each group
@@ -70,72 +139,211 @@ training_data.clean %>%
   group_by(variable) %>%
   levene_test(as.formula(paste("value ~ as.factor(", clustering, ")", sep = "")))
 
+# No homogeneity
 
-model <- lm(formula(paste0("cbind(",paste0(covariates, collapse = ","), ") ~ ", clustering, sep = "")), training_data.clean )
-Manova(model, test.statistic = "Pillai") 
 
+
+## LOOKING FOR STATISTICALLY DIFERENT VARIABLES ##
+res.man <- manova(as.formula(paste0("cbind(",paste0(covariates, collapse = ","), ") ~ factor(", clustering, ")", sep = "")), training_data.clean )
+summary(res.man)
+
+post_hoc <- summary.aov(res.man)
+for (i in seq_along(covariates)) {
+  print(paste("p-val of", covariates[i], "is", post_hoc[[i]][["Pr(>F)"]][[1]]))
+}
+
+
+summary(manova(as.formula(paste0("cbind(",paste0(covariates, collapse = ","), ") ~ ", clustering, sep = "")), training_data.clean ))
+summary(manova(as.formula(paste0("cbind(",paste0(covariates_wrong, collapse = ","), ") ~ ", clustering, sep = "")), training_data.clean ))
+
+for (cov in covariates_wrong) {
+  print(paste("p-val of", cov, "is", summary(manova(as.formula(paste0(cov, " ~ ", clustering, sep = "")), training_data.clean ))$stats[1, 4]))
+}
+
+names(aov(O_sat ~ ward_11, data = training_data.clean))
+
+oneway.test(NO2~ ward_11, data = training_data.clean)
+
+summary(aov(O_sat ~ factor(ward_11), data = training_data.clean))
 
 ## LDA ## 
+
+covariates <- c("NO3","NO2",  "Salinity", "Chla", "O_sat", "SiO4", "PO4", "T")
 lda_formula <- as.formula(
   paste(clustering, "~", paste(covariates, collapse = " + "))
 )
-LDA <- MASS::lda(lda_formula, data = training_data %>% select(all_of(c(clustering, covariates))) %>% na.omit(), CV = FALSE)
-confusion(LDA$class, training_data %>% select(all_of(c(clustering, covariates))) %>% na.omit() %>% pull(clustering))
+LDA <- MASS::lda(lda_formula, data = training_data.clean, CV = FALSE)
 
-LDA$svd
+sum(LDA$svd^2)
 
-heatmap(confusion(LDA$class, training_data %>% select(all_of(c(clustering, covariates))) %>% na.omit() %>% pull(clustering))$confusion, Colv=NA, Rowv=NA, scale='none')
-
-
-qda_formula <- as.formula(
-  paste(clustering, "~", paste(covariates, collapse = " + "))
-)
-QDA <- MASS::qda(qda_formula, data = training_data %>% select(all_of(c(clustering, covariates))) %>% na.omit(), CV = TRUE)
-
-
-confusion(QDA$class, training_data %>% select(all_of(c(clustering, covariates))) %>% na.omit() %>% pull(clustering))
-
-heatmap(confusion(QDA$class, training_data %>% select(all_of(c(clustering, covariates))) %>% na.omit() %>% pull(clustering))$confusion, Colv=NA, Rowv=NA, scale='none')
-
-
-
-
-df_lda <- data.frame(groups = na.omit(training_data %>% select(all_of(c(clustering, covariates)))) %>% pull(clustering), lda = LDA$x)
-lda_scaling <- data.frame(LD1 = 5 * LDA$scaling[,1], LD2 = 5 * LDA$scaling[,2])
+df_lda <- data.frame(training_data.clean, predict(LDA)$x)
+lda_scaling <- data.frame(LD1 = LDA$scaling[,1], LD2 = LDA$scaling[,2])
 centroids <- df_lda %>%
-  group_by(groups) %>%
-  summarize(centroid_LD1 = mean(lda.LD1), centroid_LD2 = mean(lda.LD2))
+  group_by(!!sym(clustering)) %>%
+  summarize(LD1 = mean(LD1), LD2 = mean(LD2))
 
+explained_var <- LDA$svd^2 / sum(LDA$svd^2)
+
+LDA$scaling %>% apply(1, norm, type = "2") %>% sort(decreasing = TRUE) %>% plot()
+LDA$scaling %>% apply(1, norm, type = "2") %>% sort(decreasing = TRUE)
+
+
+## PLOT WITH CENTROIDS AND ARROWS
 p <- ggplot() +
-    geom_point(data = df_lda,
-                         aes(x = lda.LD1, 
-                                 y = lda.LD2, 
-                                 col = factor(groups)), 
-                         size = 4) +
-    labs(color = "Groups") +
-    scale_color_manual(values = c("blue", "red", "green", "orange")) + 
     geom_segment(data = lda_scaling, 
                              aes(x = 0, y = 0, xend = LD1, yend = LD2), 
                              arrow = arrow(length = unit(0.3, "cm")), 
                              color = "black") + 
-    geom_point(data = centroids, 
-                         aes(x = centroid_LD1, y = centroid_LD2, col = factor(groups)), 
-                         size = 6, shape = 4) +
-    theme_classic() + # formatting the plot to make it pretty
+    geom_text(data = lda_scaling, 
+                      aes(x = LD1, y = LD2, label = rownames(lda_scaling)), 
+                      hjust = -0.5, vjust = -0.5) +
+    geom_text(data = centroids, 
+                         aes(x = LD1, y = LD2, label = rownames(centroids), colour = rownames(centroids)), 
+                         size = 6, 
+              show.legend = FALSE) +
+    theme_classic() + 
+    labs(
+      x = paste("LD1 (", round(explained_var[1] * 100, 2), "%)", sep = ""),
+      y = paste("LD2 (", round(explained_var[2] * 100, 2), "%)", sep = "")
+    ) +
     theme(axis.title = element_text(size = 18),
                 axis.text = element_text(size = 16),
                 legend.title = element_text(size = 20),
                 legend.text = element_text(size = 18))
 
+p
+
+var(training_data.clean %>% select(all_of(covariates)))
+df_lda %>% head()
+ggplot() +
+    geom_point(data = df_lda,
+                         aes(x = LD1, 
+                                 y = LD2, 
+                                 col = factor(!!sym(clustering))), 
+                         size = 3) +
+    stat_ellipse(data = df_lda, aes(x = LD1, y = LD2, fill = factor(ward_11)), level = 0.95, alpha = 0.4) +
+    labs(color = "Groups") +
+    geom_segment(data = lda_scaling, 
+                             aes(x = 0, y = 0, xend = LD1, yend = LD2), 
+                             arrow = arrow(length = unit(0.3, "cm")), 
+                             color = "black") + 
+    geom_text(data = lda_scaling, 
+                      aes(x = LD1, y = LD2, label = rownames(lda_scaling)), 
+                      hjust = -0.5, vjust = -0.5) +
+    geom_text(data = centroids, 
+                         aes(x = LD1, y = LD2, label = rownames(centroids), colour = rownames(centroids)), 
+                         size = 6, 
+              show.legend = FALSE) +
+    theme_classic() + 
+    labs(
+      x = paste("LD1 (", round(explained_var[1] * 100, 2), "%)", sep = ""),
+      y = paste("LD2 (", round(explained_var[2] * 100, 2), "%)", sep = "")
+    ) +
+    theme(axis.title = element_text(size = 18),
+                axis.text = element_text(size = 16),
+                legend.title = element_text(size = 20),
+                legend.text = element_text(size = 18))
+
+pca <- vegan::rda(training_data.clean %>% select(all_of(covariates)), SCALE = FALSE)
+
+biplot(pca, scaling = 1, type = "text")
+
+pca$CA$eig / sum(pca$CA$eig)
+
+vegan::cleanplot.pca(pca)
 
 
-confusion(predict(LDA))
 
 
-table(predict(LDA)$class, training_data$ward_4)
+## F-ratio test 
+for (removed_cov in covariates) {
+  # Create a new list of covariates excluding the current one
+  covariates_subset <- setdiff(covariates, removed_cov)
 
-predict(LDA)$class
-training_data$ward_4
-p <- klaR::partimat(as.factor(ward_4) ~ T + Salinity + O_sat + pH + Chla + NO3 + NO2 + NH4 + TN + PO4 + TP + SiO4, data = training_data, method = "lda")
+  # Define the LDA formula with the updated list of covariates
+  lda_formula <- as.formula(
+    paste(clustering, "~", paste(covariates_subset, collapse = " + "))
+  )
+  LDA <- MASS::lda(lda_formula, data = training_data.clean %>% select(all_of(c(clustering, covariates))) %>% na.omit(), CV = FALSE)
+  print(paste(
+    "F-ratio with", removed_cov, "removed is:", 
+    sum(LDA$svd^2), "with", length(LDA$svd), "components"
+  ),
+  sep = " " 
+  )
+}
 
-warnings()
+
+backward_elimination <- function(dataset, covariates, model = NULL, covariates_removed = NULL, f_ratios = NULL) {
+  if (length(covariates) == 1) {
+    return(c(model = list(model), f_ratios = list(setNames(f_ratios, covariates_removed))))
+  }
+
+  if (is.null(f_ratios)) {
+    lda_formula <- as.formula(
+      paste(clustering, "~", paste(covariates, collapse = " + "))
+    )
+    LDA <- MASS::lda(lda_formula, data = dataset, CV = FALSE)
+    f_ratios <- sum(LDA$svd^2)
+    covariates_removed <- "full_model"
+
+    backward_elimination(dataset, covariates, covariates_removed = covariates_removed, f_ratios = f_ratios)
+  }
+
+  results <- vector("double", length = length(covariates))
+  names(results) <- covariates
+
+  for (removed_cov in covariates) {
+    # Create a new list of covariates excluding the current one
+  covariates_subset <- setdiff(covariates, removed_cov)
+
+  
+  # Define the LDA formula with the updated list of covariates
+  lda_formula <- as.formula(
+    paste(clustering, "~", paste(covariates_subset, collapse = " + "))
+  )
+  LDA <- MASS::lda(lda_formula, data = dataset, CV = FALSE)
+  results[[removed_cov]] <- sum(LDA$svd^2)
+  }
+
+  cov_to_remove <- names(results)[which(results == max(results))]
+  covariates <- setdiff(covariates, cov_to_remove)
+  f_ratios <- c(f_ratios, results[[cov_to_remove]])
+  covariates_removed <- c(covariates_removed, cov_to_remove)
+  
+  backward_elimination(dataset, covariates, model = LDA, covariates_removed = covariates_removed, f_ratios = f_ratios)
+}
+
+
+
+test <- backward_elimination(training_data.clean %>% select(all_of(c(clustering, covariates))), covariates)
+
+
+plot(x = test$covariates_removed, y = test$f_ratios,xlab = "Number of covariates removed", ylab = "F-ratio", main = "F-ratio vs Number of Covariates Removed")
+
+test$f_ratios
+plot(test$f_ratios)
+plot(test$f_ratios[-length(test$f_ratios)] - test$f_ratios[-1], type = "o", xlab = "Number of covariates removed", ylab = "F-ratio", main = "F-ratio vs Number of Covariates Removed")
+
+test$f_ratios
+
+covariates <- c(
+  "T",
+  "Salinity",
+  "O_sat",
+  "pH",
+  "Chla",
+  "NO3",
+  "NO2",
+  "NH4",
+  "TN",
+  "PO4",
+  "TP",
+  "SiO4"
+)
+covariates <- c("NO2",  "Salinity", "Chla", "O_sat", "SiO4", "PO4", "T", "TN")
+lda_formula <- as.formula(
+  paste(clustering, "~", paste(covariates, collapse = " + "))
+)
+LDA <- MASS::lda(lda_formula, data = training_data.clean, CV = FALSE)
+sum(LDA$svd^2)
---
+++ Clustering/RDA.r
@@ -0,0 +1,206 @@
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(openxlsx)
+library(readxl)
+library(tibble)
+library(jsonlite)
+library(zoo)
+library(klaR)
+library(vegan)
+library(jsonlite)
+
+HOME_ <- paste(path.expand("~"), "PHD", sep = "/")
+
+env_data <- read.csv(paste(HOME_, "/ISPRA_20152017_Analysis/Create_dataset/df_chem_phys_mod_data_cleaned_long_format.csv", sep = "/")) 
+sites_taxa <- read.csv(paste(HOME_, "ISPRA_20152017_Analysis/sites_taxa_matrix.csv", sep = "/"))
+sheets <- getSheetNames(paste(HOME_, "ISPRA_20152017_Analysis/Clustering/Unknown_effect/IndVal_method_2.xlsx", sep = "/"))
+indval_list <-  lapply(sheets, function(sheet) read.xlsx(paste(HOME_, "ISPRA_20152017_Analysis/Clustering/Unknown_effect/IndVal_method_2.xlsx", sep = "/"), sheet = sheet, rowNames = TRUE))
+
+#read json file 
+params <- fromJSON(paste(HOME_ , "/ISPRA_20152017_Analysis/params.json", sep = "/"))
+seasons <- params[["seasons"]]
+seasons <- rep(names(seasons), each = 3)
+
+#create season column 
+env_data$Date <- as.Date(env_data$Date, format = "%Y-%m-%d")
+env_data <- env_data %>% mutate(Season = factor(seasons[as.integer(format(Date, "%m"))]), Year = factor(format(Date, "%Y"))) 
+
+
+for (sheet in sheets){
+    print(sheet)
+    print(indval_list[[sheet]] %>% filter(stat > 0.5) %>% group_by(index) %>% summarise(n = n()))
+}
+
+boxcox_transform <- function(values, min_val = 0.001) {
+  
+  if (any(is.na(values))) {
+    clean_values <- as.numeric(na.omit(values))
+  } else {
+     clean_values <- as.numeric(values)
+  }
+  if (min(clean_values) == 0.0) {
+    clean_values <- clean_values + min_val * 0.1
+  }
+
+  lambda <- boxcox(clean_values ~ 1, plotit = FALSE)
+  max_lambda <- lambda$x[which(lambda$y == max(lambda$y))]
+  if (max_lambda == 0) {
+    clean_values <- log(clean_values)
+  } else {
+    clean_values <- (clean_values^max_lambda - 1) / max_lambda
+  }
+  values[which(!is.na(values))] <- clean_values
+  return(values)
+}
+
+
+filter_merge_data <- function(env_data, sites_taxa, covariates, species, threshold_obs_species = 0) {
+  temp_env <- env_data %>% dplyr::select(all_of(c("Date", "id", "Season", "Year", covariates))) %>% na.omit()
+  temp_taxa <- sites_taxa %>% dplyr::select(all_of(c("Date", "id", species)))
+  temp_taxa <- temp_taxa %>% filter(rowSums(temp_taxa[, sapply(temp_taxa, is.numeric)] > 0) > threshold_obs_species) 
+
+  merged_data <- merge(
+    temp_env,
+    temp_taxa,
+    by = c("Date", "id")
+  )
+
+  return(merged_data)
+}
+
+species <- unique(unlist(sapply(sheets, function(sheet) {indval_list[[sheet]] %>% filter(stat > 0.5)%>% rownames()}, simplify = TRUE)))
+
+species_obs <- sites_taxa %>% dplyr::select(where(is.numeric)) %>% mutate(across(where(is.numeric), ~ .>0)) %>% colSums()
+species <- names(species_obs[species_obs > 170])
+
+
+### POSSIBLE CHOICES ###
+# 1) all covariates
+# 2) physical + pH + Chla 
+# 3) physical + pH + Chla + TN + SiO4
+
+covariates <- c(
+  "T",
+  "Salinity",
+  "O_sat",
+  "pH",
+  "Chla",
+  "NO3",
+  "NO2",
+  "NH4",
+  "TN",
+  "PO4",
+  "TP",
+  "SiO4"
+)
+
+all_nutrients <- c(
+  "Chla",
+  "NO3",
+  "NO2",
+  "NH4",
+  "TN",
+  "PO4",
+  "TP",
+  "SiO4"
+)
+
+test_models <- function(covariates, 
+species,
+boxcox = FALSE, 
+rem_multiv_out = FALSE, 
+threshold_obs_species = 10, 
+env_data. = env_data,
+sites_taxa. = sites_taxa,
+partial = FALSE
+) {
+  print(paste("using as covariates: ", paste(covariates, collapse = ", ")))
+
+  print(paste("applying boxcox", boxcox))
+  if (boxcox) {
+    env_data. <- env_data. %>% mutate(across(-c(Region, id, Date, Secchi_depth, Season, Year), ~ boxcox_transform(.))) 
+  }
+
+  RDA.data <- filter_merge_data(env_data., sites_taxa., covariates, species, threshold_obs_species = 10)
+  print(paste("Dim after filtering: ", paste(RDA.data %>% dim(), collapse = "x")))
+ 
+  print(paste("removing multivariate outliers:", rem_multiv_out))
+  if (rem_multiv_out) {
+    D2 <- stats::mahalanobis(RDA.data %>% dplyr::select(all_of(covariates)), colMeans(RDA.data %>% dplyr::select(all_of(covariates))), cov(RDA.data %>% dplyr::select(all_of(covariates))))
+    cut_off <- qchisq(0.95, df = length(covariates))
+    RDA.data <- RDA.data %>% filter(D2 < cut_off) 
+    print(paste("Dim after removing multivariate outliers: ", paste(RDA.data %>% dim(), collapse = "x")))
+  }
+
+  print("model with no log transformation")
+  resp <- RDA.data %>% dplyr::select(all_of(species)) %>% vegan::decostand("hellinger") %>% mutate(across(all_of(species), ~ . - mean(.)))
+  expl <- RDA.data %>% dplyr::select(all_of(covariates)) %>% decostand("standardize")
+  RDA.model <- rda(resp ~ ., expl)
+  tot.chi  <- RDA.model$CCA$tot.chi + RDA.model$CA$tot.chi
+  print(paste("constrained :" , RDA.model$CCA$tot.chi / tot.chi, seq = ""))
+  print(paste("unconstrained :" , RDA.model$CA$tot.chi / tot.chi, seq = ""))
+
+  print("model with log transformation")
+  resp <- RDA.data %>% dplyr::select(all_of(species)) %>% apply(2, function(x) {log(x+1)}) %>% vegan::decostand("hellinger") %>% data.frame() %>% mutate(across(all_of(species), ~ . - mean(.)))
+  expl <- RDA.data %>% dplyr::select(all_of(covariates)) %>% decostand("standardize")
+  RDA.model <- rda(resp ~ ., expl)
+  tot.chi  <- RDA.model$CCA$tot.chi + RDA.model$CA$tot.chi
+  print(paste("constrained :" , RDA.model$CCA$tot.chi / tot.chi, seq = ""))
+  print(paste("unconstrained :" , RDA.model$CA$tot.chi / tot.chi, seq = ""))
+}
+
+
+species <- unique(unlist(sapply(sheets, function(sheet) {indval_list[[sheet]] %>% filter(stat > 0.5)%>% rownames()}, simplify = TRUE)))
+test_models(covariates, species, boxcox = TRUE, rem_multiv_out = TRUE)
+
+species_obs <- sites_taxa %>% dplyr::select(where(is.numeric)) %>% mutate(across(where(is.numeric), ~ .>0)) %>% colSums()
+species <- names(species_obs[species_obs > 170])
+
+test_models(covariates, species, boxcox = TRUE, rem_multiv_out = TRUE)
+
+
+
+mod0 <- rda(resp ~ 1, expl)
+### forward selection: Salinity + T + TN + SiO4 + O_sat + PO4 + Chla + NH4 +      pH + NO2 + TP
+ordiR2step(mod0, RDA.model, perm.max = 200)
+### backward selection:  T + Salinity + O_sat + pH + Chla + NO3 + NO2 + NH4 + TN + PO4 + TP + SiO4
+ordistep(RDA.model, direction = "backward", perm.max = 200)
+
+
+
+
+
+
+mod0 <- rda(resp ~ 1, expl)
+### forward selection: TN + T + PO4 + Salinity + O_sat + SiO4 + TP + Chla + pH + NO2 + NH4 + NO3
+ordiR2step(mod0, RDA.model, perm.max = 200)
+### backward selection:  T + Salinity + O_sat + pH + Chla + NO3 + NO2 + NH4 + TN + PO4 + TP + SiO4
+ordistep(RDA.model, direction = "backward", perm.max = 200)
+
+
+
+
+
+## PARTIAL RDA
+resp <- RDA.data %>% dplyr::select(all_of(species)) %>% vegan::decostand("hellinger") %>% mutate(across(all_of(species), ~ . - mean(.)))
+expl <- RDA.data %>% dplyr::select(all_of(c(covariates, "Year", "Season"))) %>% dplyr::mutate(across(all_of(covariates), ~  decostand(.,"standardize"))) 
+RDA.pmodel <- rda(formula(paste("resp ~", paste(covariates, collapse = " + "), "+ Condition(Season)"), sep = " "), expl)
+summary(RDA.pmodel)
+RsquareAdj(RDA.pmodel)
+anova.cca(RDA.model, steps = 1000)
+
+
+resp <- RDA.data%>% dplyr::select(all_of(species)) %>% apply(2, function(x) {log(x+1)}) %>% vegan::decostand("hellinger") %>% data.frame() %>% mutate(across(all_of(species), ~ . - mean(.)))
+expl <- RDA.data %>% dplyr::select(all_of(c(covariates, "Year", "Season"))) %>% dplyr::mutate(across(all_of(covariates), ~  decostand(.,"standardize"))) 
+RDA.pmodel <- rda(formula(paste("resp ~", paste(covariates, collapse = " + "), "+ Condition(Season)"), sep = " "), expl)
+RsquareAdj(RDA.pmodel)
+summary(RDA.pmodel)
+
+
+summary(RDA.model)
+names(RDA.model)
+
+summary.rda
+
+RDA.model$tot.chi
