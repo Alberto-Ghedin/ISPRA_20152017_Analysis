9a4079ac3da625cd4c372fbdca58d1a645618eb8
Alberto-Ghedin <alberto.ghedin@phd.units.it>
2024-09-19 10:54:43 +0100

    computed global spatial correlation of env vars

--- Clustering/Unknown_effect/Test_spatial_autocorr.r
+++ Clustering/Unknown_effect/Test_spatial_autocorr.r
@@ -9,16 +9,17 @@ library(vegan)
 library(stats)
 library(gdistance)
 library(raster)
+library(spdep)
 
 HOME_ <- paste(path.expand("~"), "PHD", sep = "/")
 
 
 # Load the raster
-rast <- raster(paste(HOME_, "ISPRA_20152017_Analysis/Clustering/Unknown_effect/med-ogs-pft-rean-m__chl.nc", sep = "/"))
+rast <- raster(paste(HOME_, "ISPRA_20152017_Analysis/Clustering/chl_phyc.nc", sep = "/"))
 #rast <- raster(paste(HOME_, "ISPRA_20152017_Analysis/Clustering/Unknown_effect/rast_test.grd", sep = "/"))
 
 df_sites <- read.csv(paste(HOME_, "ISPRA_20152017_Analysis/Stations_info.csv", sep = "/")) 
-sites_coord <- df_sites %>% select(Longitude, Latitude) %>% as.matrix()
+sites_coord <- df_sites %>% dplyr::select(Longitude, Latitude) %>% as.matrix()
 
 
 is_water <- rast
@@ -38,6 +39,7 @@ fun_perm <- function(x){
 tr <- transition(is_water, fun_perm, directions = 16)
 trc <- geoCorrection(tr, type = "c", scl = "FALSE")
 cosDist <- gdistance::costDistance(trc, sites_coord) / 1000
+dist <- as.matrix(cosDist)
 
 cells <- cellFromXY(is_water, df_sites %>% filter(dist[,1] == Inf)  %>% dplyr::select(all_of(c("Longitude", "Latitude"))))
 is_water[cells] <- 1
@@ -47,18 +49,12 @@ trc <- geoCorrection(tr, type = "c", scl = "FALSE")
 cosDist <- gdistance::costDistance(trc, sites_coord) / 1000
 dist <- as.matrix(cosDist)
 
-dist[dist > 1000] <- Inf
-
 wij <- 1 / dist
 diag(wij) <- 0
 
 
-row.names(wij) <- df_sites$id
-colnames(wij) <- df_sites$id
 
 env_data <- read.csv(paste(HOME_, "/ISPRA_20152017_Analysis/Create_dataset/df_chem_phys_mod_data_cleaned_long_format.csv", sep = "/")) %>% dplyr::select(-c(Secchi_depth)) %>% dplyr::filter(!Region %in% c("Lazio", "Basilicata", "Calabria"))
-
-
 params <- fromJSON(paste(HOME_ , "/ISPRA_20152017_Analysis/params.json", sep = "/"))
 seasons <- params[["seasons"]]
 seasons <- rep(names(seasons), each = 3)
@@ -90,22 +86,27 @@ season_means <- env_data %>%
   ungroup() %>%
   split(.$Season)
 
-values <-  season_means$Winter[["Chla"]] %>% na.omit()
-ids <- season_means$Winter$id[!is.na(season_means$Winter[["Chla"]])]
+season_col <- rep(names(season_means), each = length(covariates))
+covariate_col <- rep(covariates, length(names(season_means))) 
+statistic_col <- rep(0, length(covariates) * length(names(season_means)))
+p_vale_col <- rep(0, length(covariates) * length(names(season_means)))
+i <- 1
+for (season in names(season_means)){
+  print(paste("Season: ", season))
+  for (covariate in covariates) {
+    print(paste("Covariate: ", covariate))
+    values <-  season_means[[season]][[covariate]] %>% na.omit() %>% as.numeric()
+    not_na <- !is.na(season_means[[season]][[covariate]])
+    ids <- season_means[[season]]$id[not_na]
+    wij_subset <- wij[ids, ids]
+    lw <- spdep::mat2listw(wij_subset, row.names = ids, style = "W", zero.policy = TRUE)
+    moran_test_result <- spdep::moran.test(values, lw, randomisation = FALSE, alternative = "two.sided")
+    statistic_col[i] <- moran_test_result$statistic
+    p_vale_col[i] <- moran_test_result$p.value
+    i <- i + 1
+    }
+}
 
+moran_statistics <- data.frame(Season = season_col, Covariate = covariate_col, Statistic = statistic_col, P_value = p_vale_col) 
 
-wij_subset <- wij[ids, ids]
-
-
-lw <- spdep::mat2listw(wij_subset, row.names = ids, style = "W")
-
-lw$style
-
-# Perform Moran's I test
-moran_test_result <- spdep::moran.test(values, lw)
-
-spdep::dnearneigh(wij_subset, d1 = 1, d2 = 1000)
-
-mat <- matrix(0, 240, 240)
-mat
-migmat.listw <- spdep::mat2listw(mat)
+moran_statistics %>% write.xlsx(paste(HOME_, "ISPRA_20152017_Analysis/Clustering/Unknown_effect/seasonal_spatial_autocorr.xlsx", sep = "/"), row.names = FALSE)
