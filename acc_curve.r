669dbaf0c01a2d6ca0b87b746c924f9c89c974ce
Alberto-Ghedin <alberto.ghedin@phd.units.it>
2024-10-16 13:42:34 +0100

    modified acc_curve and add exploration of SAr

--- acc_curve.r
+++ acc_curve.r
@@ -5,6 +5,8 @@ library(dplyr)
 library(tidyr)
 library(pals)
 library(jsonlite)
+library(gridExtra)
+library(scales)
 
 # Load data
 HOME_ <- paste(path.expand("~"), "PHD", sep = "/")
@@ -38,26 +40,97 @@ data <- read.csv(paste(HOME_, "ISPRA_20152017_Analysis/site_taxa_matrix_w_det_le
 data[1, c(1,2,3)] <- data[2, c(1,2,3)]
 data <- data[-2, ]
 is_species <- grepl("Sp", colnames(data))
-is_species
 colnames(data) <- data[1, ]
-data <- data %>%
+species_list <- colnames(data)[is_species]
+data <- data[-1, ] %>%
   mutate_at(vars(-c(1, 2, 3)), as.numeric)
 regions <- unique(data$Region)
 
 incidence_freq_list <- list()
 for (region in regions) {
+  regional_df <- data %>% filter(Region == region)
         incidence_freq_dataframe <- merge(
-            data %>% filter(Region == region) %>% summarise(n_samples = n_distinct(id, Date)),
-            data %>% filter(Region == region) %>% select(-c(id, Date, Region)) %>% summarise_all(~sum(. != 0))
+            regional_df %>% summarise(n_samples = n_distinct(id, Date)),
+            regional_df %>% select(-c(id, Date, Region)) %>% summarise_all(~sum(. != 0))
             )
         incidence_freq_dataframe <- incidence_freq_dataframe %>% .[. != 0]
         incidence_freq_list[[region]] <- incidence_freq_dataframe
 }
-
-
 res <- iNEXT(incidence_freq_list, datatype = "incidence_freq", q = 0, conf = 0.95, endpoint = 200)
 write.csv(res$iNextEst$size_based, paste(HOME_, "ISPRA_20152017_Analysis/acc_curve_all_regions_all_taxa.csv", sep = "/"))
 
+
+
+incidence_freq_list <- list()
+only_species <- data %>% select(1:3, all_of(species_list))
+for (region in regions) {
+  regional_df <- only_species %>% filter(Region == region)
+        incidence_freq_dataframe <- merge(
+            regional_df %>% summarise(n_samples = n_distinct(id, Date)),
+            regional_df %>% select(-c(id, Date, Region)) %>% summarise_all(~sum(. != 0))
+            )
+        incidence_freq_dataframe <- incidence_freq_dataframe %>% .[. != 0]
+        incidence_freq_list[[region]] <- incidence_freq_dataframe
+}
+res <- iNEXT(incidence_freq_list, datatype = "incidence_freq", q = 0, conf = 0.95, endpoint = 200)
+write.csv(res$iNextEst$size_based, paste(HOME_, "ISPRA_20152017_Analysis/acc_curve_all_regions_only_species.csv", sep = "/"))
+
+
+
+acc_curve.size_based <- read.csv(paste(HOME_, "ISPRA_20152017_Analysis/acc_curve_all_regions_only_species.csv", sep = "/")) %>% rename(Region = Assemblage) 
+
+capitalize_first <- function(string) {
+  paste0(toupper(substring(string, 1, 1)), substring(string, 2))
+}
+
+make_raref_plot_richness <- function(acc_curve, plot_title, taxon_level = "Taxa") {
+  acculumated_richness <- acc_curve %>%
+  group_by(Region) %>%
+  filter(abs(t - 50) == min(abs(t - 50))) %>% slice(1)
+  acculumated_richness$Region <- factor(acculumated_richness$Region, levels = c("FVG", "VEN", "EMR", "MAR", "ABR", "MOL", "PUG", "BAS", "CAL", "CAM","LAZ", "TOS", "LIG", "SIC", "SAR"))
+
+  custom_palette <- scales::hue_pal()(length(unique(acculumated_richness$Region)))
+  names(custom_palette) <- unique(acculumated_richness$Region)
+  line_types <- c("Rarefaction" = "solid", "Extrapolation" = "dotted")
+  p1 <- ggplot() +
+    geom_line(data = acc_curve %>% filter(Method != "Observed"), aes(x = t, y = qD, color = Region, linetype = Method), linewidth = 1) +
+    geom_ribbon(data = acc_curve, aes(x = t, y = qD, ymin = qD.LCL, ymax = qD.UCL, fill = Region), alpha = 0.2) +
+    scale_linetype_manual(values = line_types) +
+    scale_color_manual(values = custom_palette) +
+    scale_fill_manual(values = custom_palette) +
+    geom_point(data = acc_curve %>% filter(Method == "Observed"), aes(x = t, y = qD, color = Region), size = 3) +
+    theme_bw(base_size = 18) + 
+    geom_vline(xintercept = 50, linetype = "dashed", color = "black") + 
+    labs(x = "Number of samples", y = paste(capitalize_first(taxon_level), "richness", sep = " ")) +
+    theme(
+      axis.title.x = element_text(size = 20),
+      axis.title.y = element_text(size = 20),
+      axis.text.x = element_text(size = 15),
+      axis.text.y = element_text(size = 15)
+    )
+    
+  p2 <- ggplot(acculumated_richness) + 
+    geom_point(aes(x = Region, y = qD, color = Region), size = 7) +
+    geom_errorbar(aes(x = Region, ymin = qD.LCL, ymax = qD.UCL, color = Region), width = 0.5)  +
+    theme_bw(base_size = 18) +
+    scale_color_manual(values = custom_palette) +
+    labs(x = "Region", y = paste("Estimated", tolower(taxon_level), "richness", sep = " ")) +
+    theme(legend.position = "none") +
+    theme(
+      axis.title.x = element_text(size = 20),
+      axis.title.y = element_text(size = 20),
+      axis.text.x = element_text(size = 15),
+      axis.text.y = element_text(size = 15)
+    )
+  plot_path <- paste(HOME_, "ISPRA_20152017_Analysis/Plots/Rich_levels", sep = "/")
+  ggsave(paste(plot_path, plot_title, sep = "/"), grid.arrange(p1, p2, ncol = 2, widths = c(1, 1), heights = c(1)), width = 25, height = 10)
+}
+
+
+make_raref_plot_richness(acc_curve.size_based, "acc_curve_all_regions_only_species.png", "Species")
+
+
+#original plots
 N <- length(incidence_freq_list)
 ggiNEXT(res, type=1, color.var="Assemblage") + theme_bw(base_size = 18) + 
 scale_shape_manual(values=rep(19,length(incidence_freq_list))) + 
@@ -75,9 +148,3 @@ scale_colour_manual(values = unname(alphabet(N))) +
 guides(shape=FALSE)
 
 res$iNextEst$size_based %>% filter(between(SC, 0.90,0.95))
-
-
-
-
-
-
