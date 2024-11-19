12b2e3aedfe07e1dd1aed469f56f957fc5f5eb2a
Alberto Ghedin <alberto.ghedin@phd.units.it>
2024-11-19 10:12:21 +0100

    moving plots to R

--- Paper_1/plot.r
+++ Paper_1/plot.r
@@ -2,6 +2,7 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(scales)
+library(ComplexHeatmap)
 
 HOME_ <- "."
 phyto_abund <- read.csv(file.path(HOME_, "phyto_abund.csv"))
@@ -29,20 +30,65 @@ ordered_basins <- c("NorthAdr", "SouthAdr", "Ion", "SouthTyr", "NorthTyr", "West
 phyto_abund$Basin <- factor(phyto_abund$Basin, levels = ordered_basins, ordered = TRUE)
 phyto_abund$Region <- factor(phyto_abund$Region, levels = unname(from_region_to_abreviation), ordered = TRUE)
 phyto_abund$Season <- factor(phyto_abund$Season, levels = c("Winter", "Spring", "Summer", "Autumn"), ordered = TRUE)
+# Define colors for regions
+colors <- scales::hue_pal()(length(unique(phyto_abund$Region)))
+palette <- setNames(colors, unique(phyto_abund$Region))
+
 abund <- phyto_abund %>%
     group_by(Date, id) %>%
     summarise(
         Num_cell_l = sum(Num_cell_l),
         Basin = first(Basin),
         Region = first(Region), 
-        Season = first(Season)
+        Season = first(Season), 
+        Longitude = first(Longitude), 
+        Latitude = first(Latitude)
     )
 
-# Define colors for regions
-colors <- scales::hue_pal()(length(unique(abund$Region)))
-palette <- setNames(colors, unique(abund$Region))
+cat_contribution <- phyto_abund %>% 
+    group_by(Region, Det_level) %>% 
+    summarise(
+        Abund = sum(Num_cell_l)
+    ) %>% group_by(Region) %>%
+    mutate(
+        Rel_Abund = Abund / sum(Abund)
+    )
+cat_contribution$Det_level <- factor(cat_contribution$Det_level, levels = c("Species", "Genus", "Higher cat.", "Unknown"))
+p <- ggplot(cat_contribution, aes(x = Region, y = Rel_Abund, fill = Det_level)) +
+    geom_bar(stat = "identity", color = "black", linewidth = 1) +
+    labs(x = "Region", y = "Relative abundance", fill = "Identification level") +
+    theme_minimal() +
+    ggtitle("Relative abundance of each identification level to the total abundance in each region") +
+    #scale_fill_manual()
+    theme_bw() +
+    theme(
+        axis.text.x = element_text(angle = 0, hjust = 0.5, size = 15),
+        axis.text.y = element_text(size = 15),
+        axis.title.y = element_text(size = 18),
+        axis.title.x = element_text(size = 18),
+        strip.text = element_text(size = 15),
+        plot.title = element_text(size = 20, hjust = 0.5),
+        legend.text = element_text(size = 15),
+        legend.title = element_text(size = 18)
+        #legend.position = "none", 
+        #strip.text.x = element_text(size = 16), 
+        #strip.text.y = element_text(size = 16), 
+        #panel.spacing = unit(1, "lines")
+    ) 
+ggsave(file.path(HOME_, "relative_abundance_per_region.pdf"), p, width = 22, height = 13, dpi = 300)
 
 
+p <- ggplot(abund) + 
+geom_point(aes(x = Longitude, y = Num_cell_l, color = Region)) +
+    scale_y_continuous(trans="log10",breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x)))
+ggsave(file.path(HOME_, "abundance_per_longitude.pdf"), p, width = 22, height = 13, dpi = 300)
+
+p <- ggplot(abund) + 
+geom_point(aes(x = Latitude, y = Num_cell_l, color = Region)) +
+    scale_y_continuous(trans="log10",breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x)))
+ggsave(file.path(HOME_, "abundance_per_latitude.pdf"), p, width = 22, height = 13, dpi = 300)
 # Create the plot
 bloom <- data.frame(
     Basin = rep(ordered_basins, each = 4),
@@ -119,7 +165,38 @@ p <- ggplot(richness, aes(x = Region, y = Taxon, fill = Region)) +
     #scale_y_continuous(trans="log10",breaks = trans_breaks("log10", function(x) 10^x),
     #            labels = trans_format("log10", math_format(10^.x))) + 
     labs(
-        y = "Abundance [cells/L]",
-        title = "Sample abundance per basin and season"
+        y = "Taxa richness",
+        title = "Sample richness per basin and season"
     ) 
 p
+ggsave(file.path(HOME_, "richness_per_basin_season.pdf"), p, width = 22, height = 13, dpi = 300)
+
+richness_species <- phyto_abund %>% dplyr::filter(Det_level == "Species") %>%
+    group_by(Date, id) %>%
+    summarise(
+        Taxon = n(),
+        Basin = first(Basin),
+        Region = first(Region), 
+        Season = first(Season)
+    )
+
+richness_species
+richness_genera <- phyto_abund %>% dplyr::filter(Det_level == "Genus") %>%
+    group_by(Date, id) %>%
+    summarise(
+        Taxon = n(),
+        Basin = first(Basin),
+        Region = first(Region), 
+        Season = first(Season)
+    )
+
+
+IndVal <- openxlsx()
+order_species <- function(df, basins, threshold = 0.5) {
+    characteristic_species <- rownames(df)[apply(df[, basins], 1, function(x) any(x >= threshold))]
+    df_long <- reshape2::melt(df[characteristic_species, basins], id.vars = "Taxon", variable.name = "Basin", value.name = "Value")
+    df_long$Basin <- factor(df_long$Basin, levels = basins, ordered = TRUE)
+    idx <- aggregate(Value ~ Taxon, data = df_long, FUN = function(x) which.max(x))$Value
+    ordered_ids <- df_long[df_long$Value %in% idx, ][order(df_long$Basin, -df_long$Value), "Taxon"]
+    return(ordered_ids)
+}
