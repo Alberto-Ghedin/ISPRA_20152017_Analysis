6f83f2e4eeba5d091d492e7a24a670a8b4000a4f
Alberto-Ghedin <alberto.ghedin@phd.units.it>
2024-11-22 16:14:18 +0100

    passing plots to R

--- Paper_1/GLM.r
+++ Paper_1/GLM.r
@@ -7,7 +7,7 @@ library(glmmTMB)
 
 HOME_ <- "."
 phyto_abund <- read.csv(paste(HOME_, "phyto_abund.csv", sep = "/"))
-
+chem_phys <- read.csv("./df_chem_phys.csv")
 sample_abund <- phyto_abund %>% group_by(Date, id) %>% summarise(sample_abund = as.integer(sum(Num_cell_l)), Region = first(Region), Season = first(Season), Basin = first(Basin)) 
 phyto_abund %>% head()
 model_nb <-  MASS::glm.nb(sample_abund ~ Region, data = sample_abund)
@@ -35,3 +35,72 @@ taxon_abund <- phyto_abund %>% group_by(Taxon) %>% summarise(mean = mean(Num_cel
 
 
 ggplot(taxon_abund) + geom_point(aes(x = log(mean), y = log(sd))) + theme_minimal() + labs(x = "Mean", y = "SD")
+
+sample_abund
+
+merge(chem_phys %>% dplyr::select(Date, id, Salinity) %>% na.omit(), sample_abund, how = "inner", by = c("Date", "id")) %>% 
+ggplot() + geom_point(aes(x = Salinity, y = log10(sample_abund))) + theme_minimal() + labs(x = "Salinity", y = "Sample abundance")
+
+model_sal <- MASS::glm.nb(sample_abund ~ Salinity + Season + Region, data = merge(chem_phys %>% dplyr::select(Date, id, Salinity) %>% na.omit(), sample_abund, how = "inner", by = c("Date", "id")))
+
+summary(model_sal)
+res <- residuals(model_sal)
+fitted <- fitted(model_sal)
+qqnorm(res)
+qqline(res)
+
+
+plot(log10(fitted), res, xlab = "Fitted values", ylab = "Residuals", main = "Residuals vs Fitted")
+abline(h = 0, col = "red")
+
+from_region_to_abreviation <- c(
+    "Friuli-Venezia-Giulia" = "FVG",
+    "Veneto" = "VEN", 
+    "Emilia-Romagna" = "EMR",
+    "Marche" = "MAR",
+    "Abruzzo" = "ABR",
+    "Molise" = "MOL",
+    "Puglia" = "PUG",
+    "Basilicata" = "BAS",
+    "Calabria" = "CAL",
+    "Sicilia" = "SIC",
+    "Campania" = "CAM", 
+    "Lazio" = "LAZ",
+    "Toscana" = "TOS",
+    "Liguria" = "LIG",
+    "Sardegna" = "SAR"
+)
+
+ordered_basins <- c("NorthAdr", "SouthAdr", "Ion", "SouthTyr", "NorthTyr", "WestMed")
+colors <- scales::hue_pal()(length(unique(phyto_abund$Region)))
+palette <- setNames(colors, unique(phyto_abund$Region))
+data <- merge(chem_phys %>% dplyr::select(Date, id, Salinity) %>% na.omit(), sample_abund, how = "inner", by = c("Date", "id"))
+data$Region <- factor(data$Region, levels = unname(from_region_to_abreviation))
+data$Season <- factor(data$Season, levels = c("Winter", "Spring", "Summer", "Autumn"))
+data$Basin <- factor(data$Basin, levels = ordered_basins)
+p <- data %>% ggplot(aes(x = Region, y = Salinity, fill = Region)) +
+    geom_boxplot(width = 0.5, position = position_dodge("preserve")) +
+    #scale_y_log10(labels = scales::scientific) +
+    scale_fill_manual(values = palette) +
+    facet_grid(Season ~ Basin, scales = "free_x", space = "free_x") +
+    theme_bw() +
+    theme(
+        axis.text.x = element_text(angle = 0, hjust = 0.5, size = 15),
+        axis.text.y = element_text(size = 15),
+        axis.title.y = element_text(size = 18),
+        axis.title.x = element_text(size = 18),
+        strip.text = element_text(size = 15),
+        plot.title = element_text(size = 20, hjust = 0.5),
+        legend.text = element_text(size = 15),
+        legend.title = element_text(size = 18),
+        legend.position = "none", 
+        strip.text.x = element_text(size = 16), 
+        strip.text.y = element_text(size = 16), 
+        panel.spacing = unit(1, "lines")
+    ) +
+    labs(
+        y = "Abundance [cells/L]",
+        title = "Sample abundance per basin and season"
+    ) 
+
+p
--- Paper_1/plot.r
+++ Paper_1/plot.r
@@ -3,6 +3,12 @@ library(dplyr)
 library(tidyr)
 library(scales)
 library(ComplexHeatmap)
+library(readxl)
+library(tibble)
+library(gridExtra)
+library(cowplot)
+library(tidytext)
+library(circlize)
 
 HOME_ <- "."
 phyto_abund <- read.csv(file.path(HOME_, "phyto_abund.csv"))
@@ -34,6 +40,126 @@ phyto_abund$Season <- factor(phyto_abund$Season, levels = c("Winter", "Spring", 
 colors <- scales::hue_pal()(length(unique(phyto_abund$Region)))
 palette <- setNames(colors, unique(phyto_abund$Region))
 
+
+top_taxa <- read.csv("./Taxa_freq_95.csv")
+top_species <- read.csv("./Species_freq_top.csv")
+top_genera <- read.csv("./Genera_freq_top.csv")
+top_classes <- read.csv("./Classes_freq_top.csv") %>% dplyr::filter(Class != 'nan')
+
+select_and_order <- function(data, title, n_otu = 10, n_samples = 2220) {
+    data <- data[1:n_otu, ]
+    data$Frequency <- round(data[, 2] / n_samples, 2)
+    data$Taxon <- factor(data[, 1], levels = data[, 1][order(data$Frequency, decreasing = FALSE)])
+    data$type <- title
+    return(data %>% dplyr::select(Taxon, Frequency, type))
+}
+
+data <- list(
+    select_and_order(top_taxa, "Most common taxa", n_otu = 10, n_samples = 2220),
+    select_and_order(top_classes, "Most common Classes", n_otu = 10, n_samples = 2220),
+    select_and_order(top_genera, "Most common Genera", n_otu = 10, n_samples = 2220),
+    select_and_order(top_species, "Most common Species", n_otu = 10, n_samples = 2220)
+) %>% bind_rows()
+
+data$type <- factor(data$type, levels = c("Most common taxa", "Most common Classes", "Most common Genera", "Most common Species"))
+data <- data %>% arrange(type, desc(Frequency))
+data %>%  mutate(Taxon = reorder_within(Taxon, Frequency, within = type))
+plot_most_common <- function(data) {    
+    p <- ggplot(data, aes(x = Frequency, y = Taxon)) +
+        geom_point(size = 5) +
+        labs(x = "Frequency of occurrence", y = "") +
+        theme(
+            axis.text.x = element_text(size = 15, color = "black"),
+            axis.text.y = element_text(size = 15, face = "italic", color = "black"),
+            axis.title.x = element_text(size = 15), 
+            axis.title.y = element_text(size = 15),
+            strip.text = element_text(size=18, face = "bold"),
+            panel.border = element_rect(colour = "black", fill=NA, linewidth=1), 
+            strip.background = element_rect(color = "black", linewidth = 1)
+        ) +
+        scale_x_continuous(labels = scales::percent) + 
+        tidytext::scale_y_reordered() +
+        facet_wrap(~type, ncol = 2, scales = "free", labeller = label_wrap_gen(multi_line = FALSE))
+    
+    return(p)
+}
+
+pdf(file.path(HOME_, "most_common_taxa_classes_genera_species.pdf"), width = 17.1, height = 8)
+plot_most_common(data %>%  mutate(Taxon = reorder_within(Taxon, Frequency, within = type)))
+grid::grid.text(label = "(a)",
+          x=0.13, 
+          y=0.98,
+          gp=gpar(fontsize=20, col="black"))
+grid::grid.text(label = "(c)",
+          x=0.13, 
+          y=0.50,
+          gp=gpar(fontsize=20, col="black"))
+grid::grid.text(label = "(b)",
+          x=0.62, 
+          y=0.98,
+          gp=gpar(fontsize=20, col="black"))
+grid::grid.text(label = "(d)",
+          x=0.62, 
+          y=0.50,
+          gp=gpar(fontsize=20, col="black"))
+dev.off()
+
+rich_classes <- phyto_abund %>%
+    filter(Taxon != "Other phytoplankton" & Det_level == "Species") %>%
+    group_by(Class) %>%
+    summarise(n_distinct = n_distinct(Taxon)) %>%
+    arrange(desc(n_distinct)) %>% rename(Taxon = Class)
+
+rich_genera <- phyto_abund %>%
+    filter(Taxon != "Other phytoplankton" & Det_level == "Species") %>%
+    group_by(Genus) %>%
+    summarise(n_distinct = n_distinct(Taxon)) %>%
+    arrange(desc(n_distinct)) %>% rename(Taxon = Genus)
+
+select_and_order <- function(data, title, n_otu = 10) {
+    data <- data[1:n_otu, ]
+    data$type <- title
+    return(data %>% dplyr::select(Taxon, n_distinct, type))
+}
+
+data <- list(
+    select_and_order(rich_classes, "Top species-rich classes", n_otu = 10), 
+    select_and_order(rich_genera, "Top species-rich genera", n_otu = 10)
+) %>% bind_rows()
+data <- data %>%  mutate(Taxon = reorder_within(Taxon, n_distinct, within = type))
+
+plot_most_common <- function(data) {    
+    p <- ggplot(data, aes(x = n_distinct, y = Taxon)) +
+        geom_point(size = 5) +
+        labs(x = "Number of species", y = "") +
+        theme(
+            axis.text.x = element_text(size = 15, color = "black"),
+            axis.text.y = element_text(size = 15, face = "italic", color = "black"),
+            axis.title.x = element_text(size = 15), 
+            axis.title.y = element_text(size = 15),
+            strip.text = element_text(size=18, face = "bold"),
+            panel.border = element_rect(colour = "black", fill=NA, linewidth=1), 
+            strip.background = element_rect(color = "black", linewidth = 1)
+        ) +
+        tidytext::scale_y_reordered() +
+        facet_wrap(~type, ncol = 2, scales = "free", labeller = label_wrap_gen(multi_line = FALSE))
+    
+    return(p)
+}
+
+pdf(file.path(HOME_, "top_species_rich_classes_genera.pdf"), width = 12, height = 6)
+plot_most_common(data %>%  mutate(Taxon = reorder_within(Taxon, n_distinct, within = type)))
+grid::grid.text(label = "(a)",
+          x=0.11, 
+          y=0.98,
+          default.unit = "npc",
+          gp=gpar(fontsize=20, col="black"))
+grid::grid.text(label = "(b)",
+          x=0.59, 
+          y=0.98,
+          gp=gpar(fontsize=20, col="black"))
+dev.off()
+
 abund <- phyto_abund %>%
     group_by(Date, id) %>%
     summarise(
@@ -53,12 +179,26 @@ cat_contribution <- phyto_abund %>%
     mutate(
         Rel_Abund = Abund / sum(Abund)
     )
+
+cat_contribution <- phyto_abund %>% 
+    group_by(Region, Date, id, Det_level) %>% 
+    summarise(Cat_abund = sum(Num_cell_l)) %>%
+    group_by(Region,Det_level) %>%
+    summarise(mean_Abund =mean(Cat_abund)) %>%  group_by(Region, Det_level) %>% 
+    summarise(
+        tot_abund = sum(mean_Abund)
+    ) %>% group_by(Region) %>%
+    mutate(
+        rel_cont = tot_abund / sum(tot_abund)
+    )
+
 cat_contribution$Det_level <- factor(cat_contribution$Det_level, levels = c("Species", "Genus", "Higher cat.", "Unknown"))
-p <- ggplot(cat_contribution, aes(x = Region, y = Rel_Abund, fill = Det_level)) +
+p <- ggplot(cat_contribution, aes(x = Region, y = rel_cont, fill = Det_level)) +
+    scale_y_continuous(labels = scales::percent) + 
     geom_bar(stat = "identity", color = "black", linewidth = 1) +
-    labs(x = "Region", y = "Relative abundance", fill = "Identification level") +
+    labs(x = "Region", y = "Proportion of abundance", fill = "Identification level") +
     theme_minimal() +
-    ggtitle("Relative abundance of each identification level to the total abundance in each region") +
+    ggtitle("Average contribuion of each identification level to the total abundance in each region") +
     #scale_fill_manual()
     theme_bw() +
     theme(
@@ -191,12 +331,94 @@ richness_genera <- phyto_abund %>% dplyr::filter(Det_level == "Genus") %>%
     )
 
 
-IndVal <- openxlsx()
+IndVal <- read_excel("./indval_per_basin.xlsx", sheet = "Indval", col_names = TRUE) 
+colnames(IndVal)[1] <- "Taxon"
+IndVal <- IndVal %>% dplyr::filter(Taxon != "Other phytoplankton")
+
 order_species <- function(df, basins, threshold = 0.5) {
-    characteristic_species <- rownames(df)[apply(df[, basins], 1, function(x) any(x >= threshold))]
-    df_long <- reshape2::melt(df[characteristic_species, basins], id.vars = "Taxon", variable.name = "Basin", value.name = "Value")
+    characteristic_species <- df$Taxon[apply(df[, basins], 1, function(x) any(x >= threshold))]
+    df_long <- reshape2::melt(df[, c("Taxon", basins)] %>% dplyr::filter(Taxon %in% characteristic_species), id.vars = "Taxon", variable.name = "Basin", value.name = "Value")
     df_long$Basin <- factor(df_long$Basin, levels = basins, ordered = TRUE)
-    idx <- aggregate(Value ~ Taxon, data = df_long, FUN = function(x) which.max(x))$Value
-    ordered_ids <- df_long[df_long$Value %in% idx, ][order(df_long$Basin, -df_long$Value), "Taxon"]
+    ordered_ids <- df_long %>%
+    group_by(Taxon) %>%
+    summarise(Max_Basin = Basin[which.max(Value)], Max_Value = max(Value)) %>%
+    arrange(Max_Basin, desc(Max_Value)) %>% pull(Taxon)
     return(ordered_ids)
 }
+
+
+
+maps <- vector("list", 2)
+taxa_list <- order_species(IndVal, ordered_basins, threshold = 0)
+complete_indval <- IndVal %>% column_to_rownames(var = "Taxon") %>% .[taxa_list, ordered_basins] %>% as.matrix()
+max_indval <- max(complete_indval)
+col_fun = colorRamp2(c(0, max_indval / 2, max_indval), c("darkgreen", "white", "brown4"))
+maps[[1]] <- grid.grabExpr(
+    draw(
+        Heatmap(complete_indval, 
+        col = col_fun,
+        cluster_rows = FALSE,
+        cluster_columns = FALSE,
+        row_names_centered = FALSE,
+        show_column_names = TRUE,
+        show_row_names = FALSE,
+        column_names_rot = 45, 
+        row_title = "Taxon",
+        row_title_gp = gpar(fontsize = 18),
+        column_names_gp = gpar(fontsize = 18),
+        row_title_rot = 90,
+        show_heatmap_legend = FALSE
+        )
+    )
+)
+taxa_list <- order_species(IndVal, ordered_basins, threshold = 0.5)
+partial_indval <- IndVal %>% column_to_rownames(var = "Taxon") %>% .[taxa_list, ordered_basins] %>% as.matrix()
+maps[[2]] <- grid.grabExpr(
+    draw(
+        Heatmap(partial_indval, 
+        col = col_fun,
+        cluster_rows = FALSE,
+        cluster_columns = FALSE,
+        row_names_centered = FALSE,
+        show_column_names = TRUE,
+        show_row_names = TRUE,
+        column_names_rot = 45, 
+        row_names_gp = gpar(fontsize = 18),
+        column_names_gp = gpar(fontsize = 18),
+        row_title_rot = 90,
+        row_names_side = "left",
+        heatmap_legend_param = list(
+            title = "IndVal", 
+            #at = seq(0, max(partial_indval), 0.2), 
+            legend_width = unit(0.5, "inch"), 
+            legend_height = unit(4, "inch"), 
+            title_position = "topcenter",
+            title_gp = gpar(fontsize = 20, fontface = "bold"),
+            labels_gp = gpar(fontsize = 15), 
+            grid_width = unit(0.5, "inch")
+            ),
+        width = unit(8, "inch"),
+        cell_fun = function(j, i, x, y, width, height, fill) {
+            grid.text(sprintf("%.2f", partial_indval[i, j]), x, y, gp = gpar(fontsize = 18))
+        }
+        ), 
+        padding = unit(c(0.2, -2, 0.2, -7), "cm")
+    )
+)
+
+
+combined_plot <- grid.arrange(grobs = maps, ncol=2, clip=FALSE, widths = c(1, 2)) 
+text_grob_a <- grid::textGrob(label = "(a)",
+          x=0.01, 
+          y=0.98,
+          gp=gpar(fontsize=20, col="black"))
+text_grob_b <- grid::textGrob(label = "(b)",
+            x=0.425, 
+            y=0.98,
+            gp=gpar(fontsize=20, col="black"))
+grid.newpage()
+grid.draw(combined_plot)
+grid.draw(text_grob_a)
+grid.draw(text_grob_b)
+ggsave(file.path(HOME_, "IndVal_per_basin.pdf"), plot = grid.grab(), width = 22, height = 15, dpi = 600)
+
