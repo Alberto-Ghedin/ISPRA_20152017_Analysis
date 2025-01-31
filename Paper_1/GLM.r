e449dbd046bd7575aee3a1bb367c25874c9e0254
Alberto-Ghedin <alberto.ghedin@phd.units.it>
2025-01-31 19:31:06 +0100

    testing lm

--- Paper_1/GLM.r
+++ Paper_1/GLM.r
@@ -48,23 +48,98 @@ data_fit <-merge(
     ) %>% dplyr::select(-c(Date, id)) %>% 
     dplyr::filter(pH > 7, PO4 < 2)
 
+chem_phys %>% colnames()
+regression_plot_region <- function(data, var) {
+    var_sym <- sym(var)
+    data %>% dplyr::select(Region, Season, sample_abund, !!var_sym) %>% na.omit() %>% 
+    ggplot(aes(y = log10(sample_abund), x = !!var_sym, color = Region, shape = Season)) + 
+    geom_point() + facet_wrap(~Region, scales = "free") + 
+    geom_smooth(aes(x = !!var_sym, y = log10(sample_abund)), method = "lm") + 
+    labs(title = var)
+}
 
-data_fit %>% dplyr::filter(Basin == "NorthAdr") %>% pivot_longer(cols = -c(Region, Season, Basin, sample_abund), names_to = "var", values_to = "value")  %>% na.omit() %>% 
-ggplot() + 
-geom_point(aes(x = log10(value+1), y = log10(sample_abund), color = Region)) + facet_wrap(~ var, scales = "free") + theme_minimal() + labs(x = "Value", y = "Sample abundance")
-data_fit %>% dplyr::filter(Basin == "SouthAdr") %>% pivot_longer(cols = -c(Region, Season, Basin, sample_abund), names_to = "var", values_to = "value")  %>% na.omit() %>% 
-ggplot() + 
-geom_point(aes(x = log10(value+1), y = log10(sample_abund), color = Region)) + facet_wrap(~ var, scales = "free") + theme_minimal() + labs(x = "Value", y = "Sample abundance")
-data_fit %>% dplyr::filter(Basin == "Ion") %>% pivot_longer(cols = -c(Region, Season, Basin, sample_abund), names_to = "var", values_to = "value")  %>% na.omit() %>% 
-ggplot() + 
-geom_point(aes(x = log10(value+1), y = log10(sample_abund), color = Region)) + facet_wrap(~ var, scales = "free") + theme_minimal() + labs(x = "Value", y = "Sample abundance")
-data_fit %>% dplyr::filter(Basin == "SouthTyr") %>% pivot_longer(cols = -c(Region, Season, Basin, sample_abund), names_to = "var", values_to = "value")  %>% na.omit() %>% 
-ggplot() + 
-geom_point(aes(x = log10(value+1), y = log10(sample_abund), color = Region)) + facet_wrap(~ var, scales = "free") + theme_minimal() + labs(x = "Value", y = "Sample abundance")
-data_fit %>% dplyr::filter(Basin == "NorthTyr") %>% pivot_longer(cols = -c(Region, Season, Basin, sample_abund), names_to = "var", values_to = "value")  %>% na.omit() %>% 
-ggplot() + 
-geom_point(aes(x = log10(value+1), y = log10(sample_abund), color = Region)) + facet_wrap(~ var, scales = "free") + theme_minimal() + labs(x = "Value", y = "Sample abundance")
+regression_plot_region(data_fit, "Chla")
+regression_plot_region(data_fit, "DO")
+regression_plot_region(data_fit, "NH4")
+regression_plot_region(data_fit, "NO3")
+regression_plot_region(data_fit, "NO2")
+regression_plot_region(data_fit, "PO4")
+regression_plot_region(data_fit, "SiO4")
+regression_plot_region(data_fit, "Salinity")
+regression_plot_region(data_fit, "TN")
+regression_plot_region(data_fit, "TP")
+regression_plot_region(data_fit, "pH")
+regression_plot_region(data_fit, "T")
 
+season <- "Winter"
+
+append(
+    list(A = 1, b = 2), 
+    c(3, 
+    4)
+)
+data_fit %>% group_split(Season, Region) 
+var <- "Chla"
+lm_statistic <- function(data, var, spat_col = "Region") {
+    region <- data[[spat_col]][1]
+    season <- data$Season[1]
+    data_cleaned <- data %>% dplyr::select(sample_abund, !!sym(var)) %>% na.omit()
+    info <- c(Var = var, Region = region, Season = season)
+    if (nrow(data_cleaned) < 7) {
+        return(
+            data.frame(
+                Var = var, Region = region, Season = season,
+                Estimate = NA, `Pr(>|t|)` = NA
+            )
+        )
+    }
+    model <- lm(formula(paste("log10(sample_abund) ~", var)), data = data_cleaned)
+    return(
+        data.frame(
+            Var = var, Region = region, Season = season,
+            summary(model)$coefficients[2, c(1,4)] %>% as.list()
+        )
+    )
+}
+
+model <- lm(formula(paste("log10(sample_abund) ~", var)), data = data_fit)
+summary(model)$coefficients[2, c(1,4)] %>% as.list()
+
+variables <- c("Chla", "DO", "NH4", "NO3", "NO2", "PO4", "SiO4", "Salinity", "TN", "TP", "pH", "T")
+
+statistics <- bind_rows(
+    lapply(variables, function(x) {lapply(data_fit %>% group_split(Season, Region), function(y) lm_statistic(y, x)) %>% bind_rows()}) 
+    )# %>% na.omit()
+statistics$Region <- factor(statistics$Region, levels = unname(from_region_to_abreviation))
+statistics$Season <- factor(statistics$Season, levels = c("Winter", "Spring", "Summer", "Autumn"))
+statistics %>% head()
+
+
+statistics %>% mutate(Estimate = case_when(
+    Pr...t.. > 0.05 ~ NA,
+    TRUE ~ Estimate
+)) %>% 
+ggplot(aes(x = Season, y = Region, fill = Estimate)) +
+facet_wrap(~Var, scales = "free") +
+geom_tile() + labs(x = "Season", y = "Region", fill = "Estimate") + 
+scale_fill_gradient2(low = "blue", high = "red", limits = c(-1, 1))
+
+statistics <- bind_rows(
+    lapply(variables, function(x) {lapply(data_fit %>% group_split(Season, Basin), function(y) lm_statistic(y, x, spat_col = "Basin")) %>% bind_rows()}) 
+    )
+statistics$Region <- factor(statistics$Region, levels = c("NorthAdr", "SouthAdr", "Ion", "SouthTyr", "NorthTyr", "WestMed"))
+statistics$Season <- factor(statistics$Season, levels = c("Winter", "Spring", "Summer", "Autumn"))
+
+statistics %>% mutate(Estimate = case_when(
+    Pr...t.. > 0.05 ~ NA,
+    TRUE ~ Estimate
+)) %>% 
+ggplot(aes(x = Season, y = Region, fill = Estimate)) +
+facet_wrap(~Var, scales = "free") +
+geom_tile() + labs(x = "Season", y = "Region", fill = "Estimate") + 
+scale_fill_gradient2(low = "blue", high = "red", limits = c(-1, 1))
+
+lm(formula(paste("log10(sample_abund) ~", ".")), data = data_fit %>% dplyr::filter(Basin == "NorthAdr", Season == "Summer") %>% dplyr::select(sample_abund, all_of(variables))) %>% summary()
 
 data_fit %>% mutate(N_P = TN / TP) %>% dplyr::filter(N_P < 120) %>% dplyr::filter(!Region %in% c("EMR", "FVG", "VEN")) %>% ggplot() + 
 geom_point(aes(y = log10(sample_abund), x = N_P, color = Region)) + theme_minimal() 
